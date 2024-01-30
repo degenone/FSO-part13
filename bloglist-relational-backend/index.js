e75fcdd1bd5fca2fd6cebba12f42c470ab80b7a1
Tero Kilpeläinen <degenone1@gmail.com>
2024-01-30 15:04:51 +0200

    exercise 13.16 done

--- bloglist-relational-backend/index.js
+++ bloglist-relational-backend/index.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 
-const blogRouter = require('./controllers/blogs');
-const { connectToDatabase } = require('./utils/db');
 const { PORT } = require('./utils/config');
+const { connectToDatabase } = require('./utils/db');
 const { errorHandler } = require('./utils/middleware');
+const blogRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const authorsRouter = require('./controllers/authors');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
+app.use('/api/authors', authorsRouter);
 
 app.use(errorHandler);
 
--- bloglist-relational-backend/utils/middleware.js
+++ bloglist-relational-backend/utils/middleware.js
@@ -12,8 +12,7 @@ const errorHandler = (error, req, res, next) => {
             .status(400)
             .json({ error: getSequelizeErrorMessages(error) });
     } else if (error.name === 'SequelizeDatabaseError') {
-        const message = `invalid parameter(s): ${error.parameters.join(', ')}`;
-        return res.status(400).json({ error: message });
+        return res.status(400).json({ error });
     } else if (error.name === 'SequelizeUniqueConstraintError') {
         return res
             .status(400)
