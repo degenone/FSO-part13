79526bfb0f5786ac1599bb18ecd7ef19c76fac8f
Tero Kilpeläinen <degenone1@gmail.com>
2024-01-31 17:01:04 +0200

    exercise 13.22 done

--- bloglist-relational-backend/controllers/readingLists.js
+++ bloglist-relational-backend/controllers/readingLists.js
@@ -13,4 +13,21 @@ readingListRouter.post('/', getToken, async (req, res) => {
     res.json(listItem);
 });
 
+readingListRouter.put('/:id', getToken, async (req, res) => {
+    const { unread } = req.body;
+    if (unread === undefined) {
+        return res.status(400).json({ error: 'missing field: unread' });
+    }
+    const listItem = await ReadingList.findByPk(req.params.id);
+    if (listItem.userId !== req.decodedToken.id) {
+        return res.status(401).json({ error: 'permission denied' });
+    }
+    if (listItem) {
+        listItem.unread = unread;
+        await listItem.save();
+        return res.json(listItem);
+    }
+    res.status(404).json({ error: 'reading list item not found' });
+});
+
 module.exports = readingListRouter;
